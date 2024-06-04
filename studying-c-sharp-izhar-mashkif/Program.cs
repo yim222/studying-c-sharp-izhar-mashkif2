7f85c56149fa917ec8eebe93145749938b695724
lingar <yimprogramming@gmail.com>
2024-06-04 14:35:07 +0300

    task1 – part 1

--- studying-c-sharp-izhar-mashkif/Program.cs
+++ studying-c-sharp-izhar-mashkif/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using studying_c_sharp_izhar_mashkif.statics_demo;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Level1.RunMe();
